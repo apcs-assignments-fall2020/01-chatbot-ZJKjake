20bffef7c3283f488545c4b723582b4b471b80b1
jakezhang <24jzhang@athenian.org>
2021-09-01 17:14:14 -0700

    first try

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,7 +4,22 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Add some code here!
+        System.out.println("What is your name?");
+        String a=scan.nextLine();
+        System.out.println("Hi "+a+"");
+        System.out.println("How old are you？");
+        int b=scan.nextInt();
+        System.out.println("I'm "+b+" as well！");
+        System.out.println("What is your favorite number， "+a+"？");
+        int c=scan.nextInt();
+        System.out.println("I like "+c+" too！");
+        System.out.println("What is your least favorite number， "+a+"？");
+        int e=scan.nextInt();
+        System.out.println("I hate "+e+" as well！");
+        System.out.println("What time is it， "+a+"？");
+        int f=scan.nextInt();
+        System.out.println("I have to leave at "+f+"！");
+        System.out.println("Goodbye！");
 
         scan.close();
     }
